8a8a7928983ff0163d4af56f90ec12372f1087a8
Rony Moura <ronymmoura@gmail.com>
2023-06-26 00:15:57 -0300

    Fix upload

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,5 +1,4 @@
 import { writeFile } from "fs/promises";
-import fs from "fs";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
@@ -16,11 +15,7 @@ export async function POST(request: NextRequest) {
 
   // With the file data in the buffer, you can do whatever you want with it.
   // For this, we'll just write it to the filesystem in a new location
-  const path = `./tmp/${file.name}`;
-
-  if (!fs.existsSync("./tmp")) {
-    fs.mkdirSync("./tmp");
-  }
+  const path = `./${file.name}`;
 
   await writeFile(path, buffer);
   console.log(`open ${path} to see the uploaded file`);
