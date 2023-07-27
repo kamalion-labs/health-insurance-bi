ccab585969479827dfce1cea415aa0d044085219
Luis Bruno <l.brunofidelis@gmail.com>
2023-07-27 20:26:17 -0300

    Add covid timeline graph

---
+++ src/app/(app)/[idEmpresa]/covid/timeline/GraficoQuantidadeEventosPorCompetencia.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { Chart } from "@/components";
+import { Cids } from "@/lib/consts";
+import { BarSvgProps } from "@nivo/bar";
+import { Prisma } from "@prisma/client";
+import { format } from "date-fns";
+
+type EventoWithCids = Prisma.EventoGetPayload<{
+  include: { CID: true };
+}>;
+
+type DataType = {
+  Date: string;
+  Quantidade: number;
+  QuantidadeColor: string;
+};
+
+const labels = ["Quantidade"];
+
+export function GraficoQuantidadeEventosPorCompetencia({
+  data,
+}: {
+  data: EventoWithCids[];
+}) {
+  const competencias = [
+    ...new Set(data.map((x) => format(x.dataPagamento!, "MM/yyyy"))),
+  ];
+
+  const eventosCovid = data.filter((evento) =>
+    Cids.cidsCovid.includes(evento.CID.codigo)
+  );
+
+  const chartData: DataType[] = [];
+
+  competencias.forEach((comp) => {
+    const eventosCompetencia = eventosCovid.filter(
+      (x) => format(x.dataPagamento!, "MM/yyyy") === comp
+    );
+
+    if (eventosCompetencia.length !== 0) {
+      chartData.push({
+        Date: comp,
+        Quantidade: eventosCompetencia.length,
+        QuantidadeColor: "#52CD9F",
+      });
+    }
+  });
+
+  console.log(chartData);
+
+  const options: Omit<BarSvgProps<DataType>, "width" | "height"> = {
+    keys: labels,
+    indexBy: "Date",
+    groupMode: "grouped",
+    data: chartData,
+    label: "",
+  };
+
+  return <Chart.Bar {...options} />;
+}
---
+++ src/app/(app)/[idEmpresa]/covid/timeline/page.tsx
@@ -0,0 +1,58 @@
+import { Box, PageInitializer } from "@/components";
+import { prisma } from "@/lib/db/prisma";
+import { GraficoQuantidadeEventosPorCompetencia } from "./GraficoQuantidadeEventosPorCompetencia";
+
+type Props = {
+  params: {
+    idEmpresa: string;
+  };
+};
+
+export async function generateStaticParams() {
+  const empresas = await prisma.empresa.findMany();
+
+  return empresas.map((empresa) => ({ idEmpresa: empresa.id.toString() }));
+}
+
+export default async function Page({ params: { idEmpresa } }: Props) {
+  const eventos = await prisma.evento.findMany({
+    include: {
+      CID: true,
+    },
+    where: {
+      pessoa: {
+        idEmpresa: +idEmpresa,
+      },
+    },
+  });
+
+  return (
+    <div className="space-y-3 p-4">
+      <PageInitializer
+        title="Análise Temporal"
+        parentId="covid"
+        id="covidTimeline"
+      />
+
+      <div className="grid grid-cols-1 gap-3 md:grid-cols-1">
+        <Box.Root>
+          <Box.Title>
+            Quantidade de Procedimentos de Covid Por Competência
+          </Box.Title>
+
+          <Box.Content className="h-[250px]">
+            <GraficoQuantidadeEventosPorCompetencia data={eventos} />
+          </Box.Content>
+        </Box.Root>
+
+        <Box.Root>
+          <Box.Title>
+            Faturamento de Procedimentos de Covid Por Competência
+          </Box.Title>
+
+          <Box.Content className="h-[250px]">.</Box.Content>
+        </Box.Root>
+      </div>
+    </div>
+  );
+}
