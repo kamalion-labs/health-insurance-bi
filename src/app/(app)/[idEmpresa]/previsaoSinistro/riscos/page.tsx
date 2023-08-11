be3d4ebbaa605eceef6ce8dbb3dda259855b37b7
Luis Bruno <l.brunofidelis@gmail.com>
2023-08-11 18:30:09 -0300

    Add risco medio calc

--- src/app/(app)/[idEmpresa]/previsaoSinistro/riscos/page.tsx
+++ src/app/(app)/[idEmpresa]/previsaoSinistro/riscos/page.tsx
@@ -1,4 +1,4 @@
-import { Box, PageInitializer } from "@/components";
+import { Box, Card, PageInitializer } from "@/components";
 import { prisma } from "@/lib/db/prisma";
 import { TabelaRiscoDM } from "./TabelaRiscoDM";
 import { Prisma } from "@prisma/client";
@@ -32,6 +32,10 @@ export default async function Page({ params: { idEmpresa } }: Props) {
     },
   });
 
+  const pessoasMasculino = pessoas.filter((pessoa) => pessoa.sexo === "M");
+
+  const pessoasFeminino = pessoas.filter((pessoa) => pessoa.sexo === "F");
+
   return (
     <div className="space-y-5 p-5">
       <PageInitializer
@@ -39,6 +43,96 @@ export default async function Page({ params: { idEmpresa } }: Props) {
         id="risco"
         parentId="previsaoSinistro"
       />
+
+      <div className="grid grid-cols-2 gap-5">
+        <Card.Root>
+          <Card.Title>Score Médio DM</Card.Title>
+          <Card.Value>
+            {(
+              pessoas.reduce(
+                (sum, current) => sum + current.scoreDiabetes!,
+                0
+              ) / pessoas.length
+            ).toFixed(2)}
+          </Card.Value>
+        </Card.Root>
+
+        <Card.Root>
+          <Card.Title>Score Médio HAS</Card.Title>
+          <Card.Value>
+            {(
+              pessoas.reduce(
+                (sum, current) => sum + current.scoreHipertensao!,
+                0
+              ) / pessoas.length
+            ).toFixed(2)}
+          </Card.Value>
+        </Card.Root>
+      </div>
+
+      <div className="grid grid-cols-4 gap-5">
+        <Card.Root>
+          <Card.Title>Score Médio DM Masculinos</Card.Title>
+          <Card.Value>
+            {(
+              pessoasMasculino.reduce(
+                (sum, current) => sum + current.scoreDiabetes!,
+                0
+              ) / pessoas.length
+            ).toFixed(2)}
+          </Card.Value>
+        </Card.Root>
+
+        <Card.Root>
+          <Card.Title>Score Médio DM Femininos</Card.Title>
+          <Card.Value>
+            {(
+              pessoasFeminino.reduce(
+                (sum, current) => sum + current.scoreDiabetes!,
+                0
+              ) / pessoas.length
+            ).toFixed(2)}
+          </Card.Value>
+        </Card.Root>
+
+        <Card.Root>
+          <Card.Title>Score Médio HAS Masculinos</Card.Title>
+          <Card.Value>
+            {(
+              pessoasMasculino.reduce(
+                (sum, current) => sum + current.scoreHipertensao!,
+                0
+              ) / pessoas.length
+            ).toFixed(2)}
+          </Card.Value>
+        </Card.Root>
+
+        <Card.Root>
+          <Card.Title>Score Médio HAS Femininos</Card.Title>
+          <Card.Value>
+            {(
+              pessoasFeminino.reduce(
+                (sum, current) => sum + current.scoreHipertensao!,
+                0
+              ) / pessoas.length
+            ).toFixed(2)}
+          </Card.Value>
+        </Card.Root>
+      </div>
+
+      <div className="grid grid-cols-2 gap-5">
+        <Box.Root>
+          <Box.Title>Quantidade por Risco DM</Box.Title>
+
+          <Box.Content>{"."}</Box.Content>
+        </Box.Root>
+
+        <Box.Root>
+          <Box.Title>Quantidade por Risco HAS</Box.Title>
+
+          <Box.Content>{"."}</Box.Content>
+        </Box.Root>
+      </div>
 
       <Box.Root>
         <Box.Title>Risco de Diabetes Mellitus</Box.Title>
