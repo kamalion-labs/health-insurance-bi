d6cb911c45ab96aafdc9aa8a5e7cd384a66c2c45
Luis Bruno <l.brunofidelis@gmail.com>
2023-08-18 11:58:12 -0300

    Update seeds and consts

--- src/lib/consts/tuss.ts
+++ src/lib/consts/tuss.ts
@@ -1,4 +1,6 @@
 // TUSS - Terminologia Unificada da Saúde Suplementar
+
+// Informação obtida via antigo sistema BI pela Wellbe
 export const tussCovid = [
   "40314618",
   "40304906",
@@ -8,10 +10,45 @@ export const tussCovid = [
   "28040619",
 ];
 
+// Informação obtida via antigo sistema BI pela Wellbe
 export const tussSRAG = [
   "40302016",
   "40323676",
   "40404153",
   "40303684",
   "40404161",
+];
+
+// Informação obtida via antigo sistema BI pela Wellbe
+export const tussGravidez = ["40601137", "40808041", "40316149", "40303250"];
+
+// Informação obtida a partir da tabela de referência por código:
+// PROCEDIMENTOS DIAGNÓSTICOS E TERAPÊUTICOS (METODOS DIAGNOSTICOS POR IMAGEM) - RADIOGRAFIAS - OUTROS EXAMES
+// https://caurn.com.br/wp-content/uploads/2021/01/Tab.-Refer%C3%AAncia_C%C3%B3digos_Coparticipa%C3%A7%C3%A3o.pdf
+export const tussScreening = [
+  "40808017",
+  "40808025",
+  "40808033",
+  "40808041",
+  "40808050",
+  "40808114",
+  "40808122",
+  "40808130",
+  "40808149",
+  "40808157",
+  "40808165",
+  "40808173",
+  "40808181",
+  "40808190",
+  "40808203",
+  "40808211",
+  "40808220",
+  "40808238",
+  "40808246",
+  "40808254",
+  "40808262",
+  "40808270",
+  "40808289",
+  "40808297",
+  "40808300",
 ];
